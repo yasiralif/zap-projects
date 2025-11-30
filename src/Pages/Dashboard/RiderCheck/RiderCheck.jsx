7cb9e563d7f18eb0de002e841fbde1ae8a7f9fda
Yasir Arafat Alif <yasirarafatalif1@gmail.com>
2025-11-30 23:30:28 +0600

    user to rider fucntion added

--- src/Pages/Dashboard/RiderCheck/RiderCheck.jsx
+++ src/Pages/Dashboard/RiderCheck/RiderCheck.jsx
@@ -1,20 +1,79 @@
 import { useQuery } from '@tanstack/react-query';
 import React from 'react';
 import useAxios from '../../../Hooks/useAxios';
+import Swal from 'sweetalert2';
 
 const RiderCheck = () => {
     const axiosSecure = useAxios();
-    const {data:parcel=[] } = useQuery({
+    const { data: parcel = [] } = useQuery({
         queryKey: ['riders', 'pending'],
         queryFn: async () => {
             const res = await axiosSecure.get('/riders')
-            // console.log(res.data);
             return res.data
-
-
         }
     })
-    // console.log(parcel);
+    const handelApprove = (id) => {
+        const roleUpdate = {
+            status: 'rider'
+        }
+
+        Swal.fire({
+            title: "Are you Sure?",
+            text: `You Request Checking`,
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, I Agree"
+        }).then((result) => {
+            axiosSecure.patch(`/riders/${id}`, roleUpdate)
+                .then(res => {
+                    if (res.data.modifiedCount) {
+                        if (result.isConfirmed) {
+                            Swal.fire({
+
+                                title: "Confirme",
+                                text: "We are chcking your detalis",
+                                icon: "success"
+                            });
+                        }
+
+                    }
+
+                })
+        });
+
+
+    }
+
+      const handelRejects = (id) => {
+          Swal.fire({
+            title: "Are you Sure?",
+            text: `You Request Checking`,
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, I Agree"
+        }).then((result) => {
+            axiosSecure.delete(`/riders/${id}`)
+                .then(res => {
+                    console.log(res.data);
+                    // if (res.data.modifiedCount) {
+                    //     if (result.isConfirmed) {
+                    //         Swal.fire({
+
+                    //             title: "Confirme",
+                    //             text: "We are chcking your detalis",
+                    //             icon: "success"
+                    //         });
+                    //     }
+
+                    // }
+
+                })
+        });
+      }
     return (
         <div>
             <div className="overflow-x-auto">
@@ -26,32 +85,36 @@ const RiderCheck = () => {
                             <th>Email</th>
                             <th>NID Number</th>
                             <th>location</th>
-                            <th>Last Login</th>
+                            <th>Status</th>
                             <th>Action</th>
                         </tr>
                     </thead>
                     <tbody>
                         {
-                            parcel.map((d,i)=>(    <tr key={d._id}>
-                               <th>{i+1}</th>
-                            <td>{d.name}</td>
-                            <td>{d.email}</td>
-                            <td>{d.nid}</td>
-                            <td>{d?.district}</td>
-                            <td>12/16/2020</td>
-                            <td>
-                                <button className="btn btn-ghost btn-xs">details</button>
-                                <button className="btn btn-ghost mx-3 btn-xs">Approve</button>
-                                <button className="btn btn-ghost btn-xs">Rejects</button>
-                                
-                            </td>
-                         
-                        </tr>))
+                            parcel.map((d, i) => (<tr key={d._id}>
+                                <th>{i + 1}</th>
+                                <td>{d.name}</td>
+                                <td>{d.email}</td>
+                                <td>{d.nid}</td>
+                                <td>{d?.district}</td>
+                                <td>{d.status}</td>
+                                <td>
+                                    <button className="btn btn-ghost btn-xs">details</button>
+                                    <button
+                                        onClick={() => handelApprove(d._id)}
+                                        className="btn btn-ghost mx-3 btn-xs">Approve</button>
+                                    <button
+                                    onClick={()=>handelRejects(d._id)}
+                                    className="btn btn-ghost btn-xs">Rejects</button>
+
+                                </td>
+
+                            </tr>))
 
                         }
-                     
+
                     </tbody>
-                    
+
                 </table>
             </div>
         </div>
